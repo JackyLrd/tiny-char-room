6247288b9000594c5d9a5ec8c6e413848fbbb8fb
ubuntu16.04 <jacky@jacky.lan>
2018-04-18 22:55:23 +0800

    client now uses epoll the listen fds.

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -2,14 +2,14 @@
 
 CLIENTMSG::CLIENTMSG(): op(0)
 {
-	memset(user_name, 0, CLIENT_NAME_LENGTH);
-	memset(buf, 0, MSG_LENGTH);
+	memset(user_name, '\0', CLIENT_NAME_LENGTH);
+	memset(buf, '\0', MSG_LENGTH);
 }
 
 CLIENT::CLIENT(): sockfd(-1), port(0)
 {
-	memset(user_name, 0, CLIENT_NAME_LENGTH);
-	memset(&client_addr, 0, sizeof(sockaddr_in));
+	memset(user_name, '\0', CLIENT_NAME_LENGTH);
+	memset(&client_addr, '\0', sizeof(sockaddr_in));
 	next_client = NULL;
 	pre_client = NULL;
 }
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,16 +1,16 @@
 #define _GNU_SOURCE 1
-#include <stdio.h>
+#include <cstdio>
 #include <arpa/inet.h>
 #include <assert.h>
 #include <unistd.h>
-#include <poll.h>
-#include "../include/client.hpp"
-
+#include <sys/epoll.h>
 #include <string>
+#include "../include/client.hpp"
 
 int main()
 {
 	int client_fd;
+	bool server_on = false;
 	sockaddr_in server_addr;
 	CLIENTMSG sendMSG;
 	CLIENTMSG clientMSG;
@@ -29,70 +29,86 @@ int main()
 	int ret = connect(client_fd, (sockaddr*)&server_addr, sizeof(sockaddr));
 	assert(ret == 0);
 
-	// create poll fd
-	pollfd poll_fds[2];
+	server_on = true;
 
-	// set poll fd
-	poll_fds[0].fd = 0;
-	poll_fds[0].events = POLLIN;
-	poll_fds[0].revents = 0;
-	poll_fds[1].fd = client_fd;
-	poll_fds[1].events = POLLIN | POLLRDHUP;
-	poll_fds[1].revents = 0;
+	// create epoll fd
+	int epoll_fd = epoll_create(2);
+	assert(epoll_fd != -1);
+
+	// add events
+	epoll_event events[1024];
+	events[0].events = EPOLLIN;
+	events[0].data.fd = 0;
+	events[1].events = EPOLLIN | EPOLLRDHUP;
+	events[1].data.fd = client_fd;
+
+	epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &events[0]);
+	epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &events[1]);
 
 	while (1)
 	{
-		//printf("0");
-		ret = poll(poll_fds, 2, -1);
+		ret = epoll_wait(epoll_fd, events, 1024, -1);
 		if (ret < 0)
 		{
-			printf("poll failure.\n");
+			printf("epoll failure.\n");
 			break;
 		}
-		if (poll_fds[1].revents & POLLRDHUP)
+		for (int i = 0; i < ret; ++i)
 		{
-			printf("lost connection with server.\n");
-			break;
-		}
-		if (poll_fds[1].revents & POLLIN)
-		{
-			memset(&clientMSG, 0, sizeof(clientMSG));
-			int len = recv(client_fd, &clientMSG, sizeof(clientMSG), 0);
-			if (clientMSG.op == OK)
+			int fd = events[i].data.fd;
+			// if the fd is stdin
+			if (fd == 0)
 			{
-				printf("please input client name: \n");
-				fgets(sendMSG.user_name, CLIENT_NAME_LENGTH, stdin);
+				sendMSG.op = MSG;
+				read(fd, sendMSG.buf, MSG_LENGTH);
 				char* find;
-				find = strchr(sendMSG.user_name, '\n');
+				find = strchr(sendMSG.buf, '\n');
 				if (find)
-					*find = '\0';
-				sendMSG.op = USER;
+				*find = '\0';
+				// "bye" is the quit code
+				if (std::string(sendMSG.buf) == std::string("bye"))
+				{
+					sendMSG.op = EXIT;
+					send(client_fd, &sendMSG, sizeof(sendMSG), 0);
+					break;
+				}
 				send(client_fd, &sendMSG, sizeof(sendMSG), 0);
 			}
-			else if (clientMSG.op == USER)
-				printf("user %s is login.\n", clientMSG.user_name);
-			else if (clientMSG.op == EXIT)
-				printf("user %s is logout.\n", clientMSG.user_name);
-			else if (clientMSG.op == MSG)
-				printf("%s: %s\n", clientMSG.user_name, clientMSG.buf);
-		}
-		if (poll_fds[0].revents & POLLIN)
-		{
-			sendMSG.op = MSG;
-			read(poll_fds[0].fd, sendMSG.buf, MSG_LENGTH);
-			char* find;
-			find = strchr(sendMSG.buf, '\n');
-			if (find)
-			*find = '\0';
-			// "bye" is the quit code
-			if (std::string(sendMSG.buf) == std::string("bye"))
+			// if the fd is client socket
+			else
 			{
-				sendMSG.op = EXIT;
-				send(client_fd, &sendMSG, sizeof(sendMSG), 0);
-				break;
+				if (events[i].events & EPOLLRDHUP)
+				{
+					printf("lost connection with server.\n");
+					server_on = false;
+					break;
+				}
+				if (events[i].events & EPOLLIN)
+				{
+					memset(&clientMSG, 0, sizeof(clientMSG));
+					int len = recv(client_fd, &clientMSG, sizeof(clientMSG), 0);
+					if (clientMSG.op == OK)
+					{
+						printf("please input client name: \n");
+						fgets(sendMSG.user_name, CLIENT_NAME_LENGTH, stdin);
+						char* find;
+						find = strchr(sendMSG.user_name, '\n');
+						if (find)
+							*find = '\0';
+						sendMSG.op = USER;
+						send(client_fd, &sendMSG, sizeof(sendMSG), 0);
+					}
+					else if (clientMSG.op == USER)
+						printf("user %s is login.\n", clientMSG.user_name);
+					else if (clientMSG.op == EXIT)
+						printf("user %s is logout.\n", clientMSG.user_name);
+					else if (clientMSG.op == MSG)
+						printf("%s: %s\n", clientMSG.user_name, clientMSG.buf);
+				}
 			}
-			send(client_fd, &sendMSG, sizeof(sendMSG), 0);
 		}
+		if (!server_on)
+			break;
 	}
 
 	close(client_fd);
